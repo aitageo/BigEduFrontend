b13fbaaefbc0b93e0883b54f4fe5b5312d4695a8
aitageo <aitageo1980@hotmail.com>
2023-01-26 20:04:10 -0500

    creando componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {}
+  {path:'',component:AppComponent},
+  {path:'login',component:LoginComponent},
 ];
 
 @NgModule({
