054e65bf8390449bcfca576441cd75a7a5f77468
aitageo <aitageo1980@hotmail.com>
2023-02-11 00:31:31 -0500

    segunda institucion agregada

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -15,7 +15,7 @@ export class MapaComponent implements OnInit {
   ngOnInit() {
     const map = L.map('map').setView([6.2486069, -75.5742467], 12);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
+      maxZoom: 17,
       attribution: 'Map data © OpenStreetMap contributors'
     }).addTo(map);
       const marker = L.marker([6.354233968597593, -75.4983243959306],{
@@ -33,6 +33,23 @@ export class MapaComponent implements OnInit {
       })
       marker.on('mouseout',(e)=> {
         marker.closePopup();
+      });//fin presbitero
+
+      const marker2 = L.marker([6.3465208464172065, -75.50386822107099],{
+        icon: L.icon({
+          iconUrl: '../assets/img/joseMiguel.png',
+          iconSize: [80, 90],
+          iconAnchor: [22, 94],
+          popupAnchor: [-3, -76],
+        })
+      }).addTo(map);
+
+      marker2.bindPopup(`Institución Educativa José Miguel de Restrepo y Puerta`);
+      marker2.on('mouseover',(e)=> {
+        marker2.openPopup();
+      })
+      marker2.on('mouseout',(e)=> {
+        marker2.closePopup();
       })
 
 
