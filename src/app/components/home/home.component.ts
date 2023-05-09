352b04a8891e6def3c1665eb2074c9a988312dd4
aitageo <aitageo1980@hotmail.com>
2023-05-09 15:09:33 -0500

    mejorando el mapa

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,3 +13,6 @@ export class HomeComponent implements OnInit {
   }
 
 }
+
+
+
--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import { marker } from 'leaflet';
+import { HttpClient } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -10,7 +13,7 @@ import { marker } from 'leaflet';
 })
 export class MapaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
     const map = L.map('map').setView([6.2486069, -75.5742467], 12);
@@ -21,7 +24,7 @@ export class MapaComponent implements OnInit {
       const marker = L.marker([6.354233968597593, -75.4983243959306],{
         icon: L.icon({
           iconUrl: '../assets/img/presbitero.jpg',
-          iconSize: [80, 90],
+          iconSize: [60, 70],
           iconAnchor: [22, 94],
           popupAnchor: [-3, -76],
         })
@@ -39,7 +42,7 @@ export class MapaComponent implements OnInit {
       const marker2 = L.marker([6.3465208464172065, -75.50386822107099],{
         icon: L.icon({
           iconUrl: '../assets/img/joseMiguel.png',
-          iconSize: [80, 90],
+          iconSize: [50, 60],
           iconAnchor: [22, 94],
           popupAnchor: [-3, -76],
         })
@@ -54,7 +57,153 @@ export class MapaComponent implements OnInit {
       })
 
 
+//
+      const marker3 = L.marker([6.346987829036584, -75.55276662733901],{
+        icon: L.icon({
+          iconUrl: '../assets/img/escudos/AbrahanReyes.png',
+          iconSize: [80, 90],
+          iconAnchor: [22, 94],
+          popupAnchor: [-3, -76],
+        })
+      }).addTo(map);
+
+      marker3.bindPopup(`Institución Educativa Abrahan Reyes`);
+      marker3.on('mouseover',(e)=> {
+        marker3.openPopup();
+      })
+      marker3.on('mouseout',(e)=> {
+        marker3.closePopup();
+      })
+//  
+ //inicio
+ const marker4 = L.marker([6.271342698590038, -75.556547532882],{
+  icon: L.icon({
+    iconUrl: '../assets/img/escudos/AlfonsoMoraNaranjo.png',
+    iconSize: [80, 90],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+  })
+}).addTo(map);
+
+marker4.bindPopup(`Institución Educativa Alfonso Mora Naranjo`);
+marker4.on('mouseover',(e)=> {
+  marker4.openPopup();
+})
+marker4.on('mouseout',(e)=> {
+  marker4.closePopup();
+})
+//Fin   
+
+ //inicio
+ const marker5 = L.marker([6.238524665498928, -75.56262297195649],{
+  icon: L.icon({
+    iconUrl: '../assets/img/escudos/AnaDeCastrillon.png',
+    iconSize: [80, 90],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+  })
+}).addTo(map);
+
+marker5.bindPopup(`Institución Educativa Ana de Castrillon`);
+marker5.on('mouseover',(e)=> {
+  marker5.openPopup();
+})
+marker5.on('mouseout',(e)=> {
+  marker5.closePopup();
+})
+//Fin
+
+
+
+ //inicio
+ const marker6
+  = L.marker([6.296820055366712, -75.53849172308296],{
+  icon: L.icon({
+    iconUrl: '../assets/img/escudos/antonioderka.png',
+    iconSize: [80, 90],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+  })
+}).addTo(map);
+
+marker6.bindPopup(`Institución Educativa Antonio Derka`);
+marker6.on('mouseover',(e)=> {
+ marker6.openPopup();
+})
+marker6.on('mouseout',(e)=> {
+  marker6.closePopup();
+})
+//Fin
+
+
+ //inicio
+ const marker8 = L.marker([6.296709127267763, -75.57299907726852],{
+  icon: L.icon({
+    iconUrl: '../assets/img/escudos/Alfredocock.png',
+    iconSize: [80, 90],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+  })
+}).addTo(map);
+
+ marker8.bindPopup(`Institución Educativa Alfredo Cock Arango`);
+marker8.on('mouseover',(e)=> {
+  marker8.openPopup();
+})
+marker8.on('mouseout',(e)=> {
+  marker8.closePopup();
+})
+//Fin
+
+
+ //inicio
+ const marker9 = L.marker([6.211488113267962, -75.6022394543276],{
+  icon: L.icon({
+    iconUrl: '../assets/img/escudos/antonioricaurte.png',
+    iconSize: [80, 90],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+  })
+}).addTo(map);
+
+marker9.bindPopup(`Institución Educativa Antonio Ricaurte`);
+marker9.on('mouseover',(e)=> {
+  marker9.openPopup();
+})
+marker9.on('mouseout',(e)=> {
+  marker9.closePopup();
+})
+//Fin
+
+ //inicio
+ const marker10 = L.marker([6.238740234435288, -75.54994466468776],{
+  icon: L.icon({
+    iconUrl: '../assets/img/escudos/AsambleaDepartamental.png',
+    iconSize: [80, 90],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+  })
+}).addTo(map);
+
+marker10.bindPopup(`Institución Educativa Asamblea Departamental`);
+marker10.on('mouseover',(e)=> {
+  marker10.openPopup();
+})
+marker10.on('mouseout',(e)=> {
+  marker10.closePopup();
+})
+//Fin
+
+        
+}
+
+    
   }
 
 
-}
+ 
+  
+  
+
+
+
